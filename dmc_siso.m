2a0423702d95bcc02a5a009be01d2b2ccf711f99
silvagal <57956770+silvagal@users.noreply.github.com>
2021-04-12 19:23:49 -0300

    Update dmc_siso.m

--- dmc_siso.m
+++ dmc_siso.m
@@ -5,30 +5,33 @@ ledPin = 'D8';
 servoPin = 'D7';
 deltaT_blink = 0.1;
 
-%Matlab support package
+%%Matlab support package
 port = 'COM5';
 board = 'Uno';
 
-% Create arduino object with the add-on library 
+%% Create arduino object with the add-on library 
 a = arduino(port, board,'Libraries', {'Ultrasonic', 'Servo'});
 
-% Create ultrasonic object 
+%% Create ultrasonic object 
 sensor = ultrasonic(a,'D4','D5');
 
-% Create servo object 
-servo = servo(a,servoPin);
+%% Create servo object 
+%servo = servo(a,servoPin);
 
-%% Processo
-z = tf('z');
-tfz = (0.2713 *(z^(-3)))/(1 - 0.8351*(z^-1));
-
+%% Função de transferência do sistema
+s = tf('s');
+%tfs = 1/(1*s + 1);
+tfs = 18.62/(s^2);
+Ts = 1;                       
+tfz = c2d(tfs,Ts)
 %% Definir os parâmetros do controlador 
 P = 10;        %Horizonte de predição
-M = 5;         %Horizonte de controle
+M = 2;         %Horizonte de controle
 lambda = 1;    %ponderação na ação de controle
 
 %% Obter o vetor de resposta ao degrau
 g = step(tfz);    %resposta ao degrau
+plot(g);
 N = length(g);    %número de amostras de resposta ao degrau
 
 %% Cálculo da matriz G
@@ -37,34 +40,42 @@ K = (inv(G' * G + lambda * eye(M)))*(G');    %cálculo da matriz de ganho
 K_ganho = K(1,:);                            %pega a primeira linha da matriz de ganho
 
 %% Algoritmo DMC
-u(10:30) = 1;
-ym(1:30) = 0;
-referencia(1:30) = 1.6401;
-deltaU(1:30) = 0;
-duf(1:30) = 0;
+temp = 500;               %Tempo de funcionamento do sistema
+u(10:temp) = 1
+ym(1:temp) = 0;
+Nm=length(g);
+referencia(10:temp) = 10;
+deltaU(1:temp) = 0;
 somatorio = 0;
-Nm=30;
-% Obtain sensed distance
-dist = readDistance(sensor)
 
-for k = 4:1000
-    resp_livre = zeros(1,P);                 %resposta livre
-    ym(k) = 0.2713*u(k-3) - 0.8351*ym(k-1);  %saída do sistema
+vect_g = zeros(1,Nm);
+duf=zeros(1,Nm);
+
+%% Inicializa 
+%% Aplicação DMC
+for k = 3:temp
+    dist = readDistance(sensor) * 100;   %leitura do sensor ultrassonico em centímetros
+    u(k) = dist;
+    resp_livre = zeros(1,P);                                 %resposta livre
+    ym(k) = 9.31*u(k-1) + 9.31*u(k-2) + 2*ym(k-1) - ym(k-2); %saída forçada
     for kk = 1:P
-        for i = 1:Nm
+        for i = 1:(Nm-P)
             vect_g(i) = g(kk + i) - g(i);
+        end
+        for i = (Nm - P + 1) : Nm
+             vect_g(i) = g(Nm) - g(i);
         end
         resp_livre(1:P) = ym(k) + vect_g*duf';
     end
    
-    % Cálculo do Controle
-    deltaU(k) = K*(referencia(k) - resp_livre');
+    deltaU(k) = K_ganho*(referencia(k) - resp_livre');
     u(k) = u(k-1) + deltaU(k);
     aux = duf(1:end-1); 
     duf = [deltaU(k) aux];
-        %Send position to servo
-    %loop that sends the control response to the servo motor 
-    for angle = 0 : 1/180 : u(k)
+    
+    %% Send position to servo
+    for angle = 0 : 1/180 : ym(k)
         writePosition(servo, angle);
     end
 end
+plot(ym);
