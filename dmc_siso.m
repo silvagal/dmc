163498a6d0d0482a7c0c3aa591fd9b7a272de70e
silvagal <57956770+silvagal@users.noreply.github.com>
2021-04-15 21:52:25 -0300

    Update dmc_siso.m

--- dmc_siso.m
+++ dmc_siso.m
@@ -16,23 +16,21 @@ a = arduino(port, board,'Libraries', {'Ultrasonic', 'Servo'});
 sensor = ultrasonic(a,'D4','D5');
 
 %% Create servo object 
-%servo = servo(a,servoPin);
+servo = servo(a,servoPin);
 
 %% Função de transferência do sistema
 s = tf('s');
-%tfs = 1/(1*s + 1);
 tfs = 18.62/(s^2);
 Ts = 1;                       
 tfz = c2d(tfs,Ts)
 %% Definir os parâmetros do controlador 
-P = 10;        %Horizonte de predição
-M = 2;         %Horizonte de controle
-lambda = 1;    %ponderação na ação de controle
+P = 10;                                             %Horizonte de predição
+M = 2;                                              %Horizonte de controle
+lambda = 1;                                         %ponderação na ação de controle
 
 %% Obter o vetor de resposta ao degrau
-g = step(tfz);    %resposta ao degrau
-plot(g);
-N = length(g);    %número de amostras de resposta ao degrau
+g = step(tfz);                                      %resposta ao degrau
+N = length(g);                                      %número de amostras de resposta ao degrau
 
 %% Cálculo da matriz G
 G = toeplitz(g(1:P),g(1)*eye(1,M));          %cria-se uma matriz diagonal com as amostras em degrau
@@ -40,24 +38,21 @@ K = (inv(G' * G + lambda * eye(M)))*(G');    %cálculo da matriz de ganho
 K_ganho = K(1,:);                            %pega a primeira linha da matriz de ganho
 
 %% Algoritmo DMC
-temp = 500;               %Tempo de funcionamento do sistema
-u(10:temp) = 1
+temp = 100;               %Tempo de funcionamento do sistema
+u(10:temp) = 1;
 ym(1:temp) = 0;
 Nm=length(g);
-referencia(10:temp) = 10;
+referencia(10:temp) = 20;
 deltaU(1:temp) = 0;
 somatorio = 0;
-
 vect_g = zeros(1,Nm);
 duf=zeros(1,Nm);
 
-%% Inicializa 
 %% Aplicação DMC
 for k = 3:temp
-    dist = readDistance(sensor) * 100;   %leitura do sensor ultrassonico em centímetros
-    u(k) = dist;
+    dist = readDistance(sensor)*100;   %leitura do sensor ultrassonico em centímetros
     resp_livre = zeros(1,P);                                 %resposta livre
-    ym(k) = 9.31*u(k-1) + 9.31*u(k-2) + 2*ym(k-1) - ym(k-2); %saída forçada
+    ym(k) = dist;                                            %saída forçada
     for kk = 1:P
         for i = 1:(Nm-P)
             vect_g(i) = g(kk + i) - g(i);
@@ -70,12 +65,18 @@ for k = 3:temp
    
     deltaU(k) = K_ganho*(referencia(k) - resp_livre');
     u(k) = u(k-1) + deltaU(k);
-    aux = duf(1:end-1); 
+    aux = duf(1:end-1);
     duf = [deltaU(k) aux];
-    
+    y_angle = mapfun(ym(k), 0, 180, 0, 1)
     %% Send position to servo
-    for angle = 0 : 1/180 : ym(k)
+    for angle = 0 : 1/180 : y_angle
         writePosition(servo, angle);
     end
 end
-plot(ym);
+plot(u);
+title('lambda = 0.5')
+xlabel('Período de amostragem');
+%ylabel('Sinal de Controle u'); 
+ylabel('Saída do sistema');
+hold on;
+plot(referencia,'--');
